aeb9c7ecead717a86c78ee439f500cb894ef1783
Cody Bartel <cbartel@uwo.ca>
2015-03-25 20:25:57 -0400

    menu buttons added

--- src/gui/GUIHandler.java
+++ src/gui/GUIHandler.java
@@ -2,26 +2,31 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 
-public class GUIHandler extends JFrame {
+public class GUIHandler extends JFrame implements ActionListener {
 
 	private ObjectHandler m_objHandler;
+	private String mode;
 	
 	private JPanel contentPane;
 	private JFrame m_drawingFrame;
 	//Menu Bar
 	private JMenuBar menuBar;
-	private JMenu mnFile;
+	private JMenu mnFile, mnDrawOptions;
 	private JMenuItem mnMenuItem;
 	private ButtonGroup mnShapeSelect;
+	private JRadioButtonMenuItem mnScribbleButton, mnLineButton, mnRectangleButton, mnSquareButton, mnEllipseButton, mnCircleButton, mnPolygonButton;
 
 	public GUIHandler() {
 		//Initializing Components
@@ -31,16 +36,44 @@ public class GUIHandler extends JFrame {
 		//Initializing GUI Objects
 		menuBar = new JMenuBar();
 		mnFile = new JMenu("File");
+		mnDrawOptions = new JMenu("Draw Mode");
 		
-		mnMenuItem = new JMenuItem("fuck");
-		mnMenuItem.setSelected(true);
-		mnShapeSelect.add(mnMenuItem);
-		mnFile.add(mnMenuItem);
+		mnScribbleButton = new JRadioButtonMenuItem("Scribble", false);
+		mnDrawOptions.add(mnScribbleButton);
+		mnShapeSelect.add(mnScribbleButton);
 		
-		mnMenuItem = new JMenuItem("me");
-		mnMenuItem.setSelected(false);
-		mnShapeSelect.add(mnMenuItem);
-		mnFile.add(mnMenuItem);
+		mnLineButton = new JRadioButtonMenuItem("Line", false);
+		mnDrawOptions.add(mnLineButton);
+		mnShapeSelect.add(mnLineButton);
+		
+		mnRectangleButton = new JRadioButtonMenuItem("Rectangle", false);
+		mnDrawOptions.add(mnRectangleButton);
+		mnShapeSelect.add(mnRectangleButton);
+		
+		mnSquareButton = new JRadioButtonMenuItem("Square", false);
+		mnDrawOptions.add(mnSquareButton);
+		mnShapeSelect.add(mnSquareButton);
+		
+		mnEllipseButton = new JRadioButtonMenuItem("Ellipse", false);
+		mnDrawOptions.add(mnEllipseButton);
+		mnShapeSelect.add(mnEllipseButton);
+		
+		mnCircleButton = new JRadioButtonMenuItem("Circle", false);
+		mnDrawOptions.add(mnCircleButton);
+		mnShapeSelect.add(mnCircleButton);
+		
+		mnPolygonButton = new JRadioButtonMenuItem("Polygon", false);
+		mnDrawOptions.add(mnPolygonButton);
+		mnShapeSelect.add(mnPolygonButton);
+		
+		mnScribbleButton.addActionListener(this);
+		mnLineButton.addActionListener(this);
+		mnRectangleButton.addActionListener(this);
+		mnSquareButton.addActionListener(this);
+		mnEllipseButton.addActionListener(this);
+		mnCircleButton.addActionListener(this);
+		mnPolygonButton.addActionListener(this);
+		
 		
 		contentPane = new JPanel();
 		
@@ -50,6 +83,7 @@ public class GUIHandler extends JFrame {
 		//Setting menu bar
 		setJMenuBar(menuBar);
 		menuBar.add(mnFile);
+		menuBar.add(mnDrawOptions);
 		
 		//Setting content pane
 		contentPane.setLayout(new BorderLayout());
@@ -58,6 +92,12 @@ public class GUIHandler extends JFrame {
 		this.setSize(800, 600);
 		
 		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("hi");
+		
 	}
 
 }
