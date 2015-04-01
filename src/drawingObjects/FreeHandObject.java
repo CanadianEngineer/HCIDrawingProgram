e218ba30988742d797251915797b44092e0ddad4
Cody Bartel <cbartel@uwo.ca>
2015-04-01 16:44:55 -0400

    no

--- src/drawingObjects/FreeHandObject.java
+++ src/drawingObjects/FreeHandObject.java
@@ -31,6 +31,8 @@ public class FreeHandObject extends DrawingObject{
 			//Setting bottom region
 			if(p.y > m_y_end) m_y_end = (int) p.y;
 		}
+		
+		
 	}
 	
 	public void draw(Graphics g) {
@@ -45,14 +47,12 @@ public class FreeHandObject extends DrawingObject{
 		for(Point p : points){
 			p.x += x_shift;
 			p.y += y_shift;
-			
-			m_x_start += x_shift;
-			m_y_start += y_shift;
-			m_x_end += x_shift;
-			m_y_end += y_shift;
-			
-			System.out.println(m_x_start + " " + m_y_start + " " + m_x_end + " " + m_y_end);
 		}
+		
+		m_x_start += x_shift;
+		m_y_start += y_shift;
+		m_x_end += x_shift;
+		m_y_end += y_shift;
 	}
 
 }
